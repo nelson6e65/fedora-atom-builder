6b072f4847d633f89aba412413642312bebdcf2c
Nelson Martell <nelson6e65-dev@yahoo.es>
2015-06-28 20:39:17 -0430

    add: Main executable bash.
    Only install dependencies.

---
+++ run.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+
+echo "Atom builder for Fedora"
+
+if [[ -z $ROOT_DIR ]]; then
+    # Current script directory
+    declare -xr ROOT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+fi
+
+. "$ROOT_DIR/config/init"
+
+if [[ $? -ne 0 ]]; then
+    echo "Internal Error: Inicialize."
+    exit 1
+fi
+
+function bash_header() {
+    clear
+    echo_separator "#"
+    echo "# Atom Builder for Fedora"
+    echo "# by @nelson6e65         "
+    echo_separator "#"
+}
+
+
+bash_header
+
+echo_header "Directories that will be used" "-"
+dir=$(font_bold "$ROOT_DIR")
+echo "Root working directory: '$dir'"
+dir=$(font_bold "$ROOT_DIR")
+echo "Sources directory:      '$dir'"
+
+log_cd "$ROOT_DIR"
+
+script/install-dependencies
+
+pause
+
+# script/get-sources
+
+
+
+
+# pause
